4ff25598bd07d7e53c551a29559124dc3e2e7d03
Arseniy <73026795+arseniy89@users.noreply.github.com>
2022-03-23 16:13:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ onload = () => {
         ctx.setTransform()
         ctx.fillStyle = 'white'
         ctx.strokeStyle = 'black'
-        ctx.lineCap = 'round'
         ctx.lineWidth = 10
         ctx.beginPath()
         ctx.arc(canvas.width/2, canvas.height/2, canvas.width/2 - 10, 0, Math.PI*2)
@@ -28,8 +27,9 @@ onload = () => {
     }
 
     function drawHand(hand, time, color, size, len) {
-        let ang = (360/60) * time * (Math.PI/180)
-        if (hand == 'h') {ang = ang*5 + Math.abs((360/60)*(new Date().getMinutes()-30)*(Math.PI/180)/12)}
+        let ang = time * Math.PI / 30
+        if (hand == 'h') {ang = ang*5 + new Date().getMinutes() * Math.PI / 360}
+        if (hand == 'm') {ang += new Date().getSeconds() * Math.PI / 1800}
         const sin = Math.sin(ang)
         const cos = Math.cos(ang)
         ctx.setTransform(cos, sin, -sin, cos, canvas.width/2, canvas.height/2)
@@ -38,7 +38,7 @@ onload = () => {
         ctx.lineWidth = size
         ctx.beginPath()
         ctx.moveTo(0, 0)
-        ctx.lineTo(0, len - 50)
+        ctx.lineTo(0, 50 - len)
         ctx.stroke()
     }
 
@@ -47,11 +47,11 @@ onload = () => {
 
         drawWatchFace()
 
-        drawHand('h', new Date().getHours() - 30, 'black', 15, canvas.width/2 - 45)
+        drawHand('h', new Date().getHours(), 'black', 15, canvas.width/2 - 45)
 
-        drawHand('m', new Date().getMinutes() - 30, 'black', 12, canvas.width/2 - 20)
+        drawHand('m', new Date().getMinutes(), 'black', 12, canvas.width/2 - 20)
 
-        drawHand('s', new Date().getSeconds() - 30, 'red', 10, canvas.width/2 - 10)
+        drawHand('s', new Date().getSeconds(), 'red', 10, canvas.width/2 - 10)
 
         requestAnimationFrame(Watch)
     }
